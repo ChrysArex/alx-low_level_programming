2294e14a612687ec79b7edac8f5ce90a2ea0a89f
root <root@localhost.localdomain>
2023-02-20 00:28:01 +0100

    advance tasks

---
+++ 0x00-hello_world/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+* main - this is the entry point
+* Description: signe of a random number
+* Return: return 0 for success
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				if (j > i && k > j)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55)
+					putchar(44);
+					putchar(32);
+					}
+				}
+			}
+	}
+	putchar(10);
+	return (0);
+}
