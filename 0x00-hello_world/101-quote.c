c90fa76c9caf17131440e0fd2ce2ee4367e72698
root <root@localhost.localdomain>
2023-02-21 03:19:58 +0100

    advanced hello world 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - this is the entry point
+* Return: return 1 for success
+*/
+int main(void)
+{
+	char q[] = "Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "and that piece of art is useful\" - %s", q);
+	return (1);
+}
