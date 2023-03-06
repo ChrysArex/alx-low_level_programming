17babc512c321b405a33a9a1bf014150545d2aad
root <root@localhost.localdomain>
2023-03-06 16:59:11 +0100

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - find prime factor
+ * Description: finds and prints the largest prime factor of 612852475143
+ * @c: the char to check
+ * Return: 1 for success
+ */
+int main(void)
+{
+	return (0);
+}
