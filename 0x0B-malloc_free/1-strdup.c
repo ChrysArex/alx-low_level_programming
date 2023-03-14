66fe31bfb6452af59b7cd1609564cb9f68123312
root <root@localhost.localdomain>
2023-03-14 17:22:19 +0100

    malloc functions

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copy of a string
+ * Description: allocate space in memory, with a copy of the string given.
+ * @str: The string we want to copy
+ * Return: a pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *pstr;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; *(str + i) != '\0'; i++)
+		;
+	i++;
+	pstr = malloc(i);
+	for (j = 0; j < i; j++)
+		*(pstr + j) = *(str + j);
+	return (pstr);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenate two strings
+ * Description: allocate space in memory which contains the contents of s1.
+ * @s1: The first string
+ * @s2: the second string
+ * Return: a pointer to a newly allocated space in memory containing s1
+ */
+char *str_concat(char *s1, char *s2)
+{
+	
+}
