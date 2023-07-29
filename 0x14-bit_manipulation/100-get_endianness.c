d98edf6becbdaacab4c5b1ca36e05492db5cb6c7
root <root@localhost.localdomain>
2023-07-29 11:42:15 +0100

    endianness task add

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*
+#include "main.h"
+/**
  * get_endianness - checks the endianness
- *
+ * Description: checks the endianness of the system
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
