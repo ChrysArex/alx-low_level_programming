17260c5241ee2478671cfc7561977b0523aa9576
root <root@localhost.localdomain>
2023-09-09 21:38:48 +0100

    add order fixed on task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "hash_tables.h"
 /**
  * hash_table_set - adds an element to the hash table.
@@ -16,15 +17,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (strcmp(key, "") == 0)
 		return (0);
-	/*if (ht->array[index] == NULL)
-	{
-		ht->array[index]->key = malloc(sizeof(char) * (strlen(key) + 1));
-		strcpy(ht->array[index]->key, key);
-		ht->array[index]->value = malloc(sizeof(char) * (strlen(value) + 1));
-		strcpy(ht->array[index]->value, value);
-		ht->array[index]->next = NULL;
-		return (1);
-	}*/
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
@@ -35,9 +27,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->next = NULL;
 	if (ht->array[index] == NULL)
 		ht->array[index] = new;
-    else
+	else
 	{
-		if (strcmp(ht->array[index]->key, new->key) == 0)
+		if (strcmp(ht->array[index]->key, key) == 0)
 		{
 			free(ht->array[index]->value);
 			ht->array[index]->value = malloc(sizeof(char) * (strlen(value) + 1));
@@ -47,7 +39,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(new);
 			return (1);
 		}
-		ht->array[index]->next = new;
+		new->next = ht->array[index];
+		ht->array[index] = new;
 	}
 	return (1);
 }
