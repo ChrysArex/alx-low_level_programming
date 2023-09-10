3ff41140b79093d42bf025aad3d8d4041a381b6a
root <root@localhost.localdomain>
2023-09-10 19:04:46 +0100

    add order fixed on task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,14 +15,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *new;
 
+	if (ht == NULL)
+		return (0);
 	if (strcmp(key, "") == 0)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
 	new->key = malloc(sizeof(char) * (strlen(key) + 1));
+	if (new->key == NULL)
+		return (0);
 	strcpy(new->key, key);
 	new->value = malloc(sizeof(char) * (strlen(value) + 1));
+	if (new->value == NULL)
+		return (0);
 	strcpy(new->value, value);
 	new->next = NULL;
 	if (ht->array[index] == NULL)
